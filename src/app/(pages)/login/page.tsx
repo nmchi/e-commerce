c3a50aeff68638710ceaabe9332b95820cfc9da1
BARON-PC\nmchi <baron.nguyen@finantaged.com>
2024-02-26 17:04:45 +0700

    Big Update All DONE

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
+import { Gutter } from '../../_components/Gutter'
 import { RenderParams } from '../../_components/RenderParams'
 import { getMeUser } from '../../_utilities/getMeUser'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import LoginForm from './LoginForm'
 
 import classes from './index.module.scss'
-import Link from 'next/link'
-import Image from 'next/image'
 
 export default async function Login() {
   await getMeUser({
@@ -34,7 +35,7 @@ export default async function Login() {
           <RenderParams className={classes.params} />
 
           <div className={classes.formTitle}>
-            <h3>Welcom</h3>
+            <h3>Welcome</h3>
             <Image src="/assets/icons/hand.png" alt="hand" width={30} height={30} />
           </div>
 
